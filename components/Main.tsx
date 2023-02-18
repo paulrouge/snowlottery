4d7371fa777916f47986b5dc605e954c4d843fed
Paul Rouge <paulrouge@outlook.com>
2023-02-18 20:57:28 +0100

    mobile stuff

--- components/Main.tsx
+++ components/Main.tsx
@@ -7,18 +7,30 @@ const Main = () => {
     const { account } = useGlobalContext() 
   
     return (
-    <div className='w-screen h-screen'>
-        <div className='w-full h-full bg-funYellow flex flex-col justify-center items-center overflow-scroll pt-28'>
-            <div className='pt-12
-            flex flex-col items-center justify-center w-full md:text-8xl text-4xl
+    <div className='w-screen h-auto'>
+        <div className='
+        w-full h-full bg-funYellow flex flex-col 
+        justify-center items-center overflow-scroll 
+        pt-28 pb-20'>
+            <div className='
+            pt-12 flex flex-col items-center j
+            ustify-center w-full md:text-8xl text-4xl
             font-bold'>
                 the snow lottery!
             </div>
             <div className='px-2 md:text-2xl my-4 font-bold'>
                 
             </div>
-            <div className='md:text-2xl mx-4'>
-                winner is picked every sunday - check twitter @snowlottery
+            <div className='
+            flex flex-col md:flex-row md:text-2xl 
+            mx-4 justify-center items-center
+            '>
+                <div className='mx-4'>
+                    winner is picked every sunday
+                </div>
+                <div className='mx-4'>
+                    twitter @snowlottery
+                </div>
             </div>
             <div className='md:text-xl mt-8 flex px-4'>
                 {account ? 
